0418f408d1f0762176bd293a460badc32a6c142d
julien Vong <vong@enst.fr>
2011-03-01 09:26:07 +0100

    Update

--- src/GUI/FenetreJeu.java
+++ src/GUI/FenetreJeu.java
@@ -1,7 +1,17 @@
 package GUI;
 
+//import java.awt.Toolkit;
 import javax.swing.JFrame;
+import java.awt.Dimension;
 
 public class FenetreJeu extends JFrame {
+	static final long serialVersionUID = 1;
 	
+	public FenetreJeu() {
+		JFrame fenetre = new JFrame();
+		fenetre.setPreferredSize(new Dimension(1000,1000)); //à modifier plus tard
+		fenetre.setVisible(true);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+	}
 }
