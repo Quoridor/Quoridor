818e74a67745df497e13e9c24b12312446ce7002
Adrien Morvan <amorvan@enst.fr>
2011-03-01 09:44:40 +0100

    Update

--- src/Jeu/Case.java
+++ src/Jeu/Case.java
@@ -6,40 +6,54 @@ public class Case {
 	public Case(){
 	}
 	
-	private int pion;
-	private Case gauche, droite, haut, bas ; /* tableau des aretes adjacentes */
+	private int pion=0;
+	private Arete gauche, droite, haut, bas ; /* tableau des aretes adjacentes */
 	
 	
 	
-	public void setGauche( Case gauche){
+	
+	
+	
+	
+	public void setPion ( int joueur){
+		this.pion = joueur;
+	}
+	
+	public int getPion(){
+		return this.pion;
+	}
+	
+	
+	
+	public void setGauche( Arete gauche){
 		this.gauche=gauche;
 	}
 	
-	public void setDroite( Case c){
+	public void setDroite( Arete c){
 		this.droite=c;
 	}
 	
-	public void setHaut( Case c){
+	public void setHaut( Arete c){
 		this.haut=c;
 	}
 	
-	public void setBas( Case c){
+	public void setBas( Arete c){
 		this.bas=c;
 	}
 	
-	public Case getGauche(){
+	public Arete getGauche(){
 		return this.gauche;
 	}
 	
-	public Case getDroite(){
+	public Arete getDroite(){
 		return this.droite;
 	}
 	
-	public Case getHaut(){
+	public Arete getHaut(){
 		return this.haut;
 	}
 	
-	public Case getBas(){
+	public Arete getBas(){
 		return this.bas;
 	}
 	
