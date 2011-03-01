feeb443b073593c7d2e158805612fdc67bed7819
Adrien Morvan <amorvan@enst.fr>
2011-03-01 09:35:16 +0100

    Update

--- src/Jeu/Case.java
+++ src/Jeu/Case.java
@@ -11,6 +11,36 @@ public class Case {
 	
 	
 	
+	public void setGauche( Case gauche){
+		this.gauche=gauche;
+	}
 	
+	public void setDroite( Case c){
+		this.droite=c;
+	}
+	
+	public void setHaut( Case c){
+		this.haut=c;
+	}
+	
+	public void setBas( Case c){
+		this.bas=c;
+	}
+	
+	public Case getGauche(){
+		return this.gauche;
+	}
+	
+	public Case getDroite(){
+		return this.droite;
+	}
+	
+	public Case getHaut(){
+		return this.haut;
+	}
+	
+	public Case getBas(){
+		return this.bas;
+	}
 	
 }
