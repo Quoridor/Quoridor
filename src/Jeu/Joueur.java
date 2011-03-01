8acfc25eedaa961db2f5fc34b1dd52a467159dd8
Marie Valleron <valleron@sunc130-03.enst.fr>
2011-03-01 10:04:34 +0100

    Update

--- src/Jeu/Joueur.java
+++ src/Jeu/Joueur.java
@@ -2,9 +2,22 @@ package Jeu;
 
 public class Joueur {
 
+	
+	private ArrayList<Case> = new listeCasesArrivee<Case>(); 
 	private int numeroJoueur;
 	private int nombreMurs;
 	private Case position;
+	
+public Joueur(int numeroJoueur, Jeu jeu){
+	this.numeroJoueur=numeroJoueur;
+	if(numeroJoueur==1){
+		for(int j=0;j<9;j++){
+			this.listeCasesArrivee.add(jeu.getTabCase()[1][j]);
+		}
+		
+	}
+		
+	}
 
 	public int getNumeroJoueur() {
 		return numeroJoueur;
