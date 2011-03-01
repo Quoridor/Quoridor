eed603aa571ee0e8164dc0973ae0e4b93b4238d5
julien Vong <vong@enst.fr>
2011-03-01 09:52:56 +0100

    Update

--- src/GUI/FenetreJeu.java
+++ src/GUI/FenetreJeu.java
@@ -1,17 +1,23 @@
 package GUI;
 
-//import java.awt.Toolkit;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import java.awt.Dimension;
 
 public class FenetreJeu extends JFrame {
 	static final long serialVersionUID = 1;
 	
-	public FenetreJeu() {
-		JFrame fenetre = new JFrame();
-		fenetre.setPreferredSize(new Dimension(1000,1000)); //à modifier plus tard
-		fenetre.setVisible(true);
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+	public static void main(String[] arg) {
+		Toolkit boiteAOutil = Toolkit.getDefaultToolkit();
+		Dimension dim = boiteAOutil.getScreenSize();
+		int largeurEcran = dim.width;
+		int hauteurEcran = dim.height;
+		int largeurFenetre = largeurEcran/2; //à modifier
+		int hauteurFenetre = hauteurEcran/2; //à modifier
+		
+		JFrame fenetreJeu = new JFrame();
+		fenetreJeu.setSize(largeurFenetre, hauteurFenetre);
+		fenetreJeu.setLocation((largeurEcran - largeurFenetre)/2, (hauteurEcran - hauteurFenetre)/2);
+		fenetreJeu.setVisible(true);
 	}
 }
--- src/GUI/Grille.java
+++ src/GUI/Grille.java
@@ -1,7 +1,10 @@
 package GUI;
 
 import javax.swing.JPanel;
+import java.awt.font.GraphicAttribute;
 
 public class Grille extends JPanel {
-
+	public Grille() {
+		
+	}
 }
