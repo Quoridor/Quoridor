0f664f26f373080b5558417965e84e930322e1bb
julien Vong <vong@enst.fr>
2011-03-02 09:50:30 +0100

    Update

--- src/GUI/TestReseau.java
+++ src/GUI/TestReseau.java
@@ -8,14 +8,14 @@ public class TestReseau {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Jeu jeu = new Jeu(2);
+		/*Jeu jeu = new Jeu(2);
 		Reseau reseau = new Reseau("localhost", 4242, jeu);
 
 		reseau.envoyerNom("Pierre-Hugues Husson");
 		
 		System.out.println("Liste des joueurs");
 		reseau.recupererJoueurs();
-		for (String s : reseau.getJoueurs());
+		for (String s : reseau.getJoueurs());*/
 	}
 
 }
